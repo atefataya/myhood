3b0ab38032522edf76174c25df7ce1e949b1ac84
Atef H Ataya <atef.ataya@yahoo.com>
2015-12-09 09:12:45 -0800

    Initial Commit

---
+++ My-Hood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My-Hood
+//
+//  Created by Atef H Ataya on 12/9/15.
+//  Copyright © 2015 Atef H Ataya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
